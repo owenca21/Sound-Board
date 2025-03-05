8211b2651e019737845e40fa4152cb84cf1d5b11
Carson-Owen <owenca@vcu.edu>
2025-03-04 20:34:57 -0500

    Add new sounds to array

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong', 'airhorn','fart','bell'];
 
 sounds.forEach((sound) => {
     const btn = document.createElement('button');
+    
     btn.classList.add('btn');
+    
     btn.innerText = sound;
+    
+    btn.addEventListener('click', () => {
+    stopSounds();
+        document.getElementById(sound).play();
+    });
+    
     document.getElementById('buttons').appendChild(btn);
 });
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    });
+}
